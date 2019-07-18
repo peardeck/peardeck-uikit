42ec7ed34168dcf674d69ec0c587fa3035af473e
joekrill <joe@peardeck.com>
2019-07-18 15:37:01 -0400

    Add simple CallToAction component

--- packages/core/src/stories.ts
+++ packages/core/src/stories.ts
@@ -1,4 +1,5 @@
 import "./Button/Button.stories";
+import "./CallToAction/CallToAction.stories";
 import "./Collapsible/Collapsible.stories";
 import "./IconButton/IconButton.stories";
 import "./LoadingIndicator/LoadingIndicator.stories";
