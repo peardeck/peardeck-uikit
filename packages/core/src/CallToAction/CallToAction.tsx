dea20a7ed4d296aeff16010137e05e8b5f0dbd7b
joekrill <joe@peardeck.com>
2019-07-18 16:07:44 -0400

    Add engines to root package.json

--- packages/core/src/CallToAction/CallToAction.tsx
+++ packages/core/src/CallToAction/CallToAction.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { Button } from "../Button/Button";
 
 const StyledCallToAction = styled.div`
